daa54fb56d9cda524e8c4ada61cf6072ee0d280c
Hugo Moreau <hugo.moreau@roboticia.com>
2021-05-11 10:34:22 +0200

    Marche toujouyrs pas

--- Site/MyVisionia/siteweb/routes.py
+++ Site/MyVisionia/siteweb/routes.py
@@ -1,13 +1,15 @@
 #Les chemins
 
-from views import accueil, marche, parametrage, options, camera
+from views import accueil, logo, parametrage, options, camera
 
 
 def setup_routes(app):
     app.router.add_get('/', accueil)
-    app.router.add_get('/marchenormale', marche)
+    app.router.add_get('/logo', logo)
     app.router.add_get('/options', options)
     app.router.add_get('/parametrage', parametrage)
     app.router.add_get('/visualisation', camera)
+    app.router.add_static('/mincss', 'templates/css')
+    app.router.add_static('/style', 'templates/css')
 
     
--- Site/MyVisionia/siteweb/views.py
+++ Site/MyVisionia/siteweb/views.py
@@ -3,17 +3,25 @@ import jinja2
 import aiohttp_jinja2
 from aiohttp import web
 
+@aiohttp_jinja2.template('tmpl.jinja2')
+def handler(request):
+    return {'name': 'Andrew', 'surname': 'Svetlov'}
+
+
+
 async def accueil(request):
-    content = open("siteweb/templates/htmldetest.html", "r").read()
+    content = open("templates/htmldetest.html", "r").read()
     return web.Response(content_type="text/html", text=content)
-async def marche(request):
-    return web.Response(text='Page de mise en marche du capteur')
+async def logo(request):
+    content = open("templates/logo.html", "r").read()
+    return web.Response(content_type="text/html", text=content)
 async def parametrage(request):
     return web.Response(text='Page de paramétrage du capteur')
 async def options(request):
     return web.Response(text='Page d\'options de l\'application')
 async def camera(request):
     return web.Response(text='On va tenter')
+
 
 
 
