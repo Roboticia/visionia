cf83b0da8b1154d168a429773e5fa24652e6f631
HugoMoreaupro <75309014+HugoMoreaupro@users.noreply.github.com>
2021-05-24 12:34:28 +0200

    Update camera.py

--- Site/MyVisionia/siteweb/camera.py
+++ Site/MyVisionia/siteweb/camera.py
@@ -46,26 +46,3 @@ class CamVideoStreamTrack(VideoStreamTrack):
         frame.time_base = time_base
         self.counter += 1
         return frame
-
-
-
-
-def allumage(Expo):
-    #Init camera
-    cmd1 = 'v4l2-ctl -d 0 -c exposure='+str(Expo)
-    cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
-    arducam_utils = ArducamUtils(0)
-    cap.set(cv2.CAP_PROP_CONVERT_RGB, arducam_utils.convert2rgb)
-    
-
-    # Aquisition des dimentions de l'image en provenance du capteur
-    w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    logging.debug('Caméra initialisée')
-
-    # needed to purge the frame with default exposure
-    for i in range(6):
-        subprocess.call(cmd1, shell=True)
-        ret, frame = cap.read()
-    logging.debug('Temps d\'exposition réglé\n Allumage effectué')
-    return [cmd1,cap]
