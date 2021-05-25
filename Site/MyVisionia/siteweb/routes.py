8a2e7d9d1afeffc6c769afec049c2e3cc613dd9e
HugoMoreaupro <75309014+HugoMoreaupro@users.noreply.github.com>
2021-05-25 10:22:53 +0200

    Update routes.py

--- Site/MyVisionia/siteweb/routes.py
+++ Site/MyVisionia/siteweb/routes.py
@@ -1,6 +1,6 @@
 #Les chemins
 
-from views import accueil, logo, parametrage, options, television, index, javascript, offer,camera
+from views import accueil, logo, parametrage, options, television, index, javascript, offer,television
 
 
 def setup_routes(app):
@@ -8,7 +8,7 @@ def setup_routes(app):
     app.router.add_get('/logo', logo)
     app.router.add_get('/options', options)
     app.router.add_get('/parametrage', parametrage)
-    app.router.add_get('/visualisation', camera)
+    app.router.add_get('/visualisation', television)
     app.router.add_static('/stockage', 'templates/LEdossier')
     app.router.add_get("/video", index)
     app.router.add_get("/client.js", javascript)
