5c115dbeea852f3cb12fcf9bd2dc1c7691b6211f
HugoMoreaupro <75309014+HugoMoreaupro@users.noreply.github.com>
2021-05-25 09:49:14 +0200

    Update main.py
    test

--- Site/MyVisionia/siteweb/main.py
+++ Site/MyVisionia/siteweb/main.py
@@ -10,27 +10,6 @@ from utils import ArducamUtils
 import subprocess
 
 
-def allumage(Expo):
-    #Init camera
-    cmd1 = 'v4l2-ctl -d 0 -c exposure='+str(Expo)
-    cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
-    arducam_utils = ArducamUtils(0)
-    cap.set(cv2.CAP_PROP_CONVERT_RGB, arducam_utils.convert2rgb)
-    
-
-    # Aquisition des dimentions de l'image en provenance du capteur
-    w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    logging.debug('Caméra initialisée')
-
-    # needed to purge the frame with default exposure
-    for i in range(6):
-        subprocess.call(cmd1, shell=True)
-        ret, frame = cap.read()
-    logging.debug('Temps d\'exposition réglé\n Allumage effectué')
-    return [cmd1,cap]
-
-
 logging.basicConfig(level=logging.DEBUG)
 
 ROOT = os.path.dirname(__file__)
