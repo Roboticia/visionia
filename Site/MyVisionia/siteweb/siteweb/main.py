54d5b313ad7e294ec8667d32ba2919487d4f71ac
HugoMoreaupro <hugo-moreau@live.fr>
2021-06-21 14:56:33 +0200

    Stable

---
+++ Site/MyVisionia/siteweb/siteweb/main.py
@@ -0,0 +1,30 @@
+import logging
+from aiohttp import web
+import os
+import aiohttp_jinja2
+import jinja2
+import cv2
+from camera import allumage, Expo
+import subprocess
+from routes import setup_routes
+
+logging.basicConfig(level=logging.DEBUG)
+#print(logging.root.level)
+ROOT = os.path.dirname(__file__)
+
+
+
+
+
+logging.basicConfig(level=logging.DEBUG)
+print(logging.root.level)
+
+logging.debug('SET EFFECTUE')
+
+app = web.Application()
+setup_routes(app)
+aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates/'))
+allumage(Expo)
+
+if __name__ == "__main__":
+    web.run_app(app)
