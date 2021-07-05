722e5a14ed201068de813ee9b7285849950b7c2d
HugoMoreaupro <hugo-moreau@live.fr>
2021-07-05 14:39:08 +0200

    Add manquant sur le dernier commit

--- Site/MyVisionia/siteweb/camera.py
+++ Site/MyVisionia/siteweb/camera.py
@@ -16,9 +16,9 @@ import logging
 from av import VideoFrame
 from utils import ArducamUtils
 from aiohttp import web
-
 from aiortc.contrib.media import MediaPlayer
-logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig(level=logging.CRITICAL,filename="cam.log", filemode="a", format='%(asctime)s - %(levelname)s - %(message)s')
 
 Expo=1000
 Lumino=50
@@ -47,13 +47,13 @@ def allumage(Expo):
     #Init camera
     cap.set(cv2.CAP_PROP_CONVERT_RGB, arducam_utils.convert2rgb)
     # Aquisition des dimentions de l'image en provenance du capteur
-    logging.debug('Caméra initialisée')
+    logging.debug('Camera initialisee')
 
     # needed to purge the frame with default exposure
     for i in range(6):
         subprocess.call(cmd1, shell=True)
         ret, frame = cap.read()
-    logging.debug('Temps d\'exposition réglé\n Allumage effectué')
+    logging.debug('Temps d\'exposition regle\n Allumage effectue')
     
 
 class CamVideoStreamTrack(VideoStreamTrack):
@@ -65,52 +65,63 @@ class CamVideoStreamTrack(VideoStreamTrack):
         self.frames = []
         self.mode=mode
         self.cptImages=0
-        self.moduloEcriture=80
+        self.moduloEcriture=Freeram
     async def recv(self):
+        start_time=time.time() #uniquement pour les loggers
+        logging.critical('%3f : Debut de cycle',time.time() - start_time)
         ret, frame = cap.read()
+        logging.critical('%3f : Lecture de la frame OK', time.time() - start_time)
         frame = arducam_utils.convert(frame)
-
+        logging.critical('%3f : Conversion arducam OK', time.time() - start_time)
 
         if self.mode == 'usual':
             cv2.imwrite('cam.jpg',frame)
+            logging.critical('%3f : Ecriture disque OK', time.time() - start_time)
             img = cv2.imread('cam.jpg')
-            if self.counter%(Freeram)==(0):
+            logging.critical('%3f : Lecture disque OK', time.time() - start_time)
+            """if self.counter%(Freeram)==(0):
                 self.frames=[]
-                logging.debug('Liste videe')
+                logging.critical('%3f : Liste vide OK', time.time() - start_time)
             self.frames.append(VideoFrame.from_ndarray(numpy.array(img)))
-            logging.debug('Frame dans la liste')
+            logging.critical('%3f : Transtypee et envoyee OK', time.time() - start_time)
             pts, time_base = await self.next_timestamp()
             frame = self.frames[self.counter%(Freeram)]
-            logging.debug('Frame lue')
+            logging.critical('%3f : Conversion arducam OK', time.time() - start_time)
             logging.debug(self.counter)
             frame.pts = pts
             frame.time_base = time_base
             self.counter += 1
+            logging.critical('%3f : Fin de cycle', time.time() - start_time)"""
 
         if self.mode == 'memory':
 
             if (self.counter-1)%self.moduloEcriture:
-                #cv2.imwrite('test.jpg', frame)
-                #img = cv2.imread('test.jpg')
                 a= 'images/'+("{:0=6}".format(self.cptImages)) + '.jpg'
-                print (a)
+                logging.critical('image ecrite sous le nom : %s',a)
                 cv2.imwrite((a), frame)
+                logging.critical('%3f : Ecriture disque OK', time.time() - start_time)
+
                 img = cv2.imread(a)
+                logging.critical('%3f : Lecture disque OK', time.time() - start_time)
                 self.cptImages+=1
 
             else:
                 cv2.imwrite('cam.jpg', frame)
+                logging.critical('%3f : Ecriture disque OK', time.time() - start_time)
                 img = cv2.imread('cam.jpg')
-            if self.counter%(Freeram)==(0):
-                self.frames=[]
-                logging.debug('Liste videe')
-            self.frames.append(VideoFrame.from_ndarray(numpy.array(img)))
-            logging.debug('Frame dans la liste')
-            pts, time_base = await self.next_timestamp()
-            frame = self.frames[self.counter%(Freeram)]
-            logging.debug('Frame lue')
-            logging.debug(self.counter)
-            frame.pts = pts
-            frame.time_base = time_base
-            self.counter += 1
+                logging.critical('%3f : Lecture disque OK', time.time() - start_time)
+        if self.counter%(Freeram)==(0):
+            self.frames=[]
+            logging.critical('%3f : Liste vide OK', time.time() - start_time)
+        self.frames.append(VideoFrame.from_ndarray(numpy.array(img)))
+        logging.critical('%3f : Transtypee et envoyee OK', time.time() - start_time)
+        pts, time_base = await self.next_timestamp()
+        frame = self.frames[self.counter%(Freeram)]
+        logging.critical('%3f : Conversion arducam OK', time.time() - start_time)
+
+        logging.debug(self.counter)
+        frame.pts = pts
+        frame.time_base = time_base
+        self.counter += 1
+        logging.critical('%3f : Fin de cycle', time.time() - start_time)
         return frame
