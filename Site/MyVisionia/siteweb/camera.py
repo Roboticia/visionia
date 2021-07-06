1a858a61077482daec7123a1dca7c4e6204b0a8e
HugoMoreaupro <hugo-moreau@live.fr>
2021-07-06 19:15:01 +0200

    Optimisé : sans tableau de frames et sans ecritures

--- Site/MyVisionia/siteweb/camera.py
+++ Site/MyVisionia/siteweb/camera.py
@@ -18,7 +18,7 @@ from utils import ArducamUtils
 from aiohttp import web
 from aiortc.contrib.media import MediaPlayer
 
-logging.basicConfig(level=logging.CRITICAL,filename="cam.log", filemode="a", format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.CRITICAL)
 
 Expo=1000
 Lumino=50
@@ -62,7 +62,7 @@ class CamVideoStreamTrack(VideoStreamTrack):
     def __init__(self,mode='display'):
         super().__init__()  # don't forget this!
         self.counter = 0
-        self.frames = []
+        self.frame = VideoFrame
         self.mode=mode
         self.cptImages=0
         self.moduloEcriture=Freeram
@@ -72,26 +72,12 @@ class CamVideoStreamTrack(VideoStreamTrack):
         ret, frame = cap.read()
         logging.critical('%3f : Lecture de la frame OK', time.time() - start_time)
         frame = arducam_utils.convert(frame)
+        # img = frame
         logging.critical('%3f : Conversion arducam OK', time.time() - start_time)
 
         if self.mode == 'usual':
-            cv2.imwrite('cam.jpg',frame)
-            logging.critical('%3f : Ecriture disque OK', time.time() - start_time)
-            img = cv2.imread('cam.jpg')
-            logging.critical('%3f : Lecture disque OK', time.time() - start_time)
-            """if self.counter%(Freeram)==(0):
-                self.frames=[]
-                logging.critical('%3f : Liste vide OK', time.time() - start_time)
-            self.frames.append(VideoFrame.from_ndarray(numpy.array(img)))
-            logging.critical('%3f : Transtypee et envoyee OK', time.time() - start_time)
-            pts, time_base = await self.next_timestamp()
-            frame = self.frames[self.counter%(Freeram)]
-            logging.critical('%3f : Conversion arducam OK', time.time() - start_time)
-            logging.debug(self.counter)
-            frame.pts = pts
-            frame.time_base = time_base
-            self.counter += 1
-            logging.critical('%3f : Fin de cycle', time.time() - start_time)"""
+            img = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+            logging.critical('%3f : Changement de numpy array', time.time() - start_time)
 
         if self.mode == 'memory':
 
@@ -100,7 +86,6 @@ class CamVideoStreamTrack(VideoStreamTrack):
                 logging.critical('image ecrite sous le nom : %s',a)
                 cv2.imwrite((a), frame)
                 logging.critical('%3f : Ecriture disque OK', time.time() - start_time)
-
                 img = cv2.imread(a)
                 logging.critical('%3f : Lecture disque OK', time.time() - start_time)
                 self.cptImages+=1
@@ -110,16 +95,12 @@ class CamVideoStreamTrack(VideoStreamTrack):
                 logging.critical('%3f : Ecriture disque OK', time.time() - start_time)
                 img = cv2.imread('cam.jpg')
                 logging.critical('%3f : Lecture disque OK', time.time() - start_time)
-        if self.counter%(Freeram)==(0):
-            self.frames=[]
-            logging.critical('%3f : Liste vide OK', time.time() - start_time)
-        self.frames.append(VideoFrame.from_ndarray(numpy.array(img)))
+
+        self.frame = VideoFrame.from_ndarray(numpy.array(img))
         logging.critical('%3f : Transtypee et envoyee OK', time.time() - start_time)
         pts, time_base = await self.next_timestamp()
-        frame = self.frames[self.counter%(Freeram)]
+        frame = self.frame
         logging.critical('%3f : Conversion arducam OK', time.time() - start_time)
-
-        logging.debug(self.counter)
         frame.pts = pts
         frame.time_base = time_base
         self.counter += 1
