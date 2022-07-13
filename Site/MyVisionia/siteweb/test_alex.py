62437ed2fa50549c1e84dd948107e34f2d7e963e
alexnicolo73 <alex.nicolo@le-campus-numerique.fr>
2022-07-13 06:56:47 +0000

    error Segmentation fault (core dumped) venant probablement de ctypes

--- Site/MyVisionia/siteweb/test_alex.py
+++ Site/MyVisionia/siteweb/test_alex.py
@@ -1,11 +1,29 @@
-import darknet
+import darknet.darknet as dn
+import cv2 as cv2
+
+def prepare_batch(images, network, channels=3):
+    width = dn.network_width(network)
+    height = dn.network_height(network)
+
+    darknet_images = []
+    for image in images:
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_resized = cv2.resize(image_rgb, (width, height),
+                                   interpolation=cv2.INTER_LINEAR)
+        custom_image = image_resized.transpose(2, 0, 1)
+        darknet_images.append(custom_image)
+
+    batch_array = np.concatenate(darknet_images, axis=0)
+    batch_array = np.ascontiguousarray(batch_array.flat, dtype=np.float32)/255.0
+    darknet_images = batch_array.ctypes.data_as(darknet.POINTER(darknet.c_float))
+    return darknet.IMAGE(width, height, channels, darknet_images)
 
 def image_detection(image_or_path, network, class_names, class_colors, thresh):
     # Darknet doesn't accept numpy images.
     # Create one with image we reuse for each detect
-    width = darknet.network_width(network)
-    height = darknet.network_height(network)
-    darknet_image = darknet.make_image(width, height, 3)
+    width = dn.network_width(network)
+    height = dn.network_height(network)
+    darknet_image = dn.make_image(width, height, 3)
 
     if type(image_or_path) == "str":
         image = cv2.imread(image_or_path)
@@ -15,10 +33,10 @@ def image_detection(image_or_path, network, class_names, class_colors, thresh):
     image_resized = cv2.resize(image_rgb, (width, height),
                                interpolation=cv2.INTER_LINEAR)
 
-    darknet.copy_image_from_bytes(darknet_image, image_resized.tobytes())
-    detections = darknet.detect_image(network, class_names, darknet_image, thresh=thresh)
-    darknet.free_image(darknet_image)
-    image = darknet.draw_boxes(detections, image_resized, class_colors)
+    dn.copy_image_from_bytes(darknet_image, image_resized.tobytes())
+    detections = dn.detect_image(network, class_names, darknet_image, thresh=thresh)
+    dn.free_image(darknet_image)
+    image = dn.draw_boxes(detections, image_resized, class_colors)
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB), detections
 
 def load_network(config_file, data_file, weights, batch_size=1):
@@ -33,15 +51,16 @@ def load_network(config_file, data_file, weights, batch_size=1):
         class_names
         class_colors
     """
-    network = load_net_custom(
+    network = dn.load_net_custom(
         config_file.encode("ascii"),
         weights.encode("ascii"), 0, batch_size)
-    metadata = load_meta(data_file.encode("ascii"))
+    metadata = dn.load_meta(data_file.encode("ascii"))
     class_names = [metadata.names[i].decode("ascii") for i in range(metadata.classes)]
-    colors = class_colors(class_names)
+    colors = dn.class_colors(class_names)
     return network, class_names, colors
 
+#test_img = prepare_batch("/NNvision/darknet/data/dog.jpg","/NNvision/darknet/cfg/yolov4-tiny.cfg",channels=3)
+img = cv2.imread("/NNvision/darknet/data/dog.jpg")
 
-
-network, classNames, classColors = load_network("NNvision/darknet/cfg/yolov4-tiny.cfg", "NNvision/darknet/cfg/coco.data", "NNvision/darknet/tiny.weights")
-image_detection("NNvision/darknet/data/dog.jpg", "NNvision/darknet/cfg/yolov4-tiny.cfg", classNames, classColors, 0.2)
+network, classNames, classColors = dn.load_network("/NNvision/darknet/cfg/yolov4-tiny.cfg", "/NNvision/darknet/cfg/coco.data", "/NNvision/darknet/tiny.weights")
+image_detection(img, "/NNvision/darknet/cfg/yolov4-tiny.cfg", classNames, classColors, 0.2)
